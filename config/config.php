4ab6491fdfa1667ebc5f92fd87d1258097ab2a6f
Richard Angapin <rangapin@gmx.com>
2019-11-18 12:37:38 +0700

    adding css on login&register

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ session_start();
 
 $timezone = date_default_timezone_set("Asia/Bangkok");
 
-$con = mysqli_connect("localhost", "root", "", "social"); //Connection variable
+$con = mysqli_connect("localhost", "root", "", "TheBook"); //Connection variable
 
 if(mysqli_connect_errno())
 {
